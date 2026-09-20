8bc43aba62c7f2de10b6aaefb531e4126797df49
Akash Kamat <akash.kamat.10@gmail.com>
2026-09-20 19:15:19 +0530

    Fix CORS headers for Cloudflare Pages cross-origin requests
    - Add Access-Control-Allow-Origin: * to all API responses
    - Move CORS headers to top of handler (before all responses)
    - Fixes CORS error when calling Vercel APIs from jevllm.pages.dev

--- api/pipeline.js
+++ api/pipeline.js
@@ -35,10 +35,12 @@ function questionMeta() {
 }
 
 module.exports = async (req, res) => {
+  // Set CORS headers for all responses
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   if (req.method === "OPTIONS") {
-    res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     return res.status(204).end();
   }
 
